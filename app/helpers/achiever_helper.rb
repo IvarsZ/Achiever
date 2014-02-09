1bd2d31b3d20eb25ab9ff01f8ba04948de0500c0
Neil Hutch <neilhutch4@gmail.com>
2014-02-09 12:17:16 +0000

    Scheduling Emails

--- app/helpers/achiever_helper.rb
+++ app/helpers/achiever_helper.rb
@@ -1,12 +1,47 @@
 module AchieverHelper
 
-  def schedule_new_confirmation(date, achievment_id)
+
+  def schedule_new_confirmation(date, achievement_id)
+	#g_type = Achievment.find(achievement_id).goal.goal_type
+	#case g_type
+#		when "WakeUpGoal"
+		##	date = WakeUpGoal.find(link_id).goal_date
+	#	when "ExerciseGoal"
+           #date = ExerciseGoal.find(link_id).goal_date
+	#end
+
+	ActionMailer::Base.mail(:from => "achiever.app.ef@gmail.com", :to => "achiever.app.ef@gmail.com", :subject => "test_before_scheduler", :body => "test").deliver
+	scheduler = Rufus::Scheduler.new
+	puts date
+	scheduler.at  date do
+		#Call method to ask ref for confirmation
+		puts "IUASDOI:ASJKRNSEFLIKJ"
+		email_referee(achievement_id)
+	end
 
   end
 
   private
 
-    def method1
+    def email_referee(achievement_id)
+		#ask for confirmation from the referee
+		#code to send out yes/no confirmation message via
+		#person being judeged, get goals, stakes, referee id, 
+		#compact and form message
+		#User.find(1).fb_id
+		#email = Achievment.find(achievement_id).referee_email
 
+
+		#referee = Achievment.find(achievement_id).referee_id
+		#achiever = Achievment.find(achievement_id).user_id
+		#goals = Achievment.find(achievement_id).goal
+		#message = "Hello " + referee + ", has your friend " + achiever + " completed his goal of: " + goals
+
+		#MyMailer::deliver_mail("neilhutch4@gmail.com")
+		ActionMailer::Base.mail(:from => "achiever.app.ef@gmail.com", :to => "achiever.app.ef@gmail.com", :subject => "test schedule", :body => "test").deliver
+		puts "test"
     end
+
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -6,13 +6,16 @@ AchieverRails::Application.initialize!
 
 # Setup for sending emails to the referees
 
-#ActionMailer::Base.delivery_method = :smtp
-#ActionMailer::Base.server_settings = {
-#    :address => "smtp.tutorialspoint.com",
-#    :port => 25,
-#    :domain => "tutorialspoint.com",
-#    :authentication => :login,
-#    :user_name => "username",
-#    :password => "password",
-#}
-#ActionMailer::Base.default_content_type = "text/html"
+ActionMailer::Base.delivery_method = :smtp 
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = { 
+	:enable_starttls_auto => true,  
+	:address 		 => "smtp.gmail.com",   
+	:port            => 587,
+   # :tls             => true,
+    :authentication  => :plain,
+   # :domain => ''
+    :user_name       => 'achiever.app.ef@gmail.com',
+    :password        => 'achiever123'
+}
