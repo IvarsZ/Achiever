260fab6cebaa33d37697cce5e38caa1898dc3b0d
IvarsZ <Ivars.Zubkans@gmail.com>
2014-02-09 04:54:50 +0000

    error fix

--- app/models/stakes/wall_post_stake.rb
+++ app/models/stakes/wall_post_stake.rb
@@ -3,7 +3,7 @@ class WallPostStake < ActiveRecord::Base
 
   def post
     @user = self.stake.achievment.user
-    @graph = Koala:Facebook:API.new(user.fb_token)
+    @graph = Koala::Facebook::API.new(user.fb_token)
     @graph.put_wall_post(self.stake_str)
   end
 end
